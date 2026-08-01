b4ec9cace94abc450c8fd608bd27f77d9f3c077a
Sk Inventory <demolay541@gmail.com>
2026-08-01 17:13:07 +0900

    Debug camera error

--- src/services/cameraService.js
+++ src/services/cameraService.js
@@ -1,27 +1,29 @@
 import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
 
 export async function pickPhoto() {
-
-  const photo = await Camera.getPhoto({
-    quality: 80,
-    allowEditing: false,
-    resultType: CameraResultType.Uri,
-    source: CameraSource.Photos
-  });
-
-  return photo;
-
+  try {
+    return await Camera.getPhoto({
+      quality: 80,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Photos
+    });
+  } catch (err) {
+    alert(JSON.stringify(err));
+    throw err;
+  }
 }
 
 export async function takePhoto() {
-
-  const photo = await Camera.getPhoto({
-    quality: 80,
-    allowEditing: false,
-    resultType: CameraResultType.Uri,
-    source: CameraSource.Camera
-  });
-
-  return photo;
-
+  try {
+    return await Camera.getPhoto({
+      quality: 80,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+  } catch (err) {
+    alert(JSON.stringify(err));
+    throw err;
+  }
 }
