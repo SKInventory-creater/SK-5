79601f8f369cbe3a3884783dc772f1c7b107cf47
Sk Inventory <demolay541@gmail.com>
2026-08-11 16:00:12 +0900

    fix photo upload

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -9,17 +9,29 @@ export async function saveItemPhoto(photo, itemId) {
   const response = await fetch(photo.webPath);
   const blob = await response.blob();
 
+  console.log(
+    "PHOTO SIZE:",
+    Math.round(blob.size / 1024),
+    "KB",
+    "TYPE:",
+    blob.type
+  );
+
   const fileName = `items/${itemId}_${Date.now()}.jpg`;
 
   const storageRef = ref(storage, fileName);
+
+  console.log("PHOTO UPLOAD START:", fileName);
 
   await uploadBytes(storageRef, blob, {
     contentType: "image/jpeg"
   });
 
+  console.log("PHOTO UPLOAD FINISHED:", fileName);
+
   const downloadURL = await getDownloadURL(storageRef);
 
-  console.log("PHOTO UPLOAD OK:", downloadURL);
+  console.log("PHOTO URL READY:", downloadURL);
 
   return downloadURL;
 }
